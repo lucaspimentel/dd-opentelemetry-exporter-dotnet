1ba783a6340eee180e83f19199928f6bca14e7ff
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2022-03-31 15:57:29 -0400

    add project AspNetCoreMvc60

---
+++ samples/AspNetCoreMvc60/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AspNetCoreMvc60.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
