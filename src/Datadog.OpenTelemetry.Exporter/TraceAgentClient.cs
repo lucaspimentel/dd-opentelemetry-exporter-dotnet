341c33c516aec95ba8a7149bda1326a8fb78e8fe
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2022-04-01 18:05:38 -0400

    add missing "application/msgpack" content header

--- src/Datadog.OpenTelemetry.Exporter/TraceAgentClient.cs
+++ src/Datadog.OpenTelemetry.Exporter/TraceAgentClient.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Datadog.OpenTelemetry.Exporter.MessagePack;
 using MessagePack;
@@ -13,6 +14,11 @@ namespace Datadog.OpenTelemetry.Exporter
     {
         private const string TracesPath = "/v0.4/traces";
 
+        private readonly MessagePackSerializerOptions? _serializerOptions = MessagePackSerializerOptions.Standard
+                                                                                                        .WithResolver(SpanFormatterResolver.Instance)
+                                                                                                        .WithOmitAssemblyVersion(true);
+
+        private readonly MediaTypeHeaderValue _mediaTypeHeaderValue = new("application/msgpack");
         private readonly Uri _tracesEndpoint;
         private readonly HttpClient _client;
 
@@ -21,28 +27,28 @@ namespace Datadog.OpenTelemetry.Exporter
             _tracesEndpoint = new Uri(new Uri(baseEndpoint), TracesPath);
 
             _client = new HttpClient();
-            _client.DefaultRequestHeaders.Add(AgentHttpHeaderNames.Language, ".NET");
+            _client.DefaultRequestHeaders.Add(AgentHttpHeaderNames.Language, "dotnet");
             _client.DefaultRequestHeaders.Add(AgentHttpHeaderNames.LanguageInterpreter, RuntimeInformationWrapper.Name);
             _client.DefaultRequestHeaders.Add(AgentHttpHeaderNames.LanguageVersion, RuntimeInformationWrapper.ProductVersion);
         }
 
-        public async Task SendTracesAsync(IEnumerable<Span> spanModels)
+        public async Task SendTracesAsync(IEnumerable<Span> spans)
         {
-            if (spanModels == null)
+            if (spans == null)
             {
-                throw new ArgumentNullException(nameof(spanModels));
+                throw new ArgumentNullException(nameof(spans));
             }
 
-            var traces = (from span in spanModels
+            var traces = (from span in spans
                           group span by span.TraceId
                           into g
                           select g.ToList()).ToList();
 
-            var serializerOptions = MessagePackSerializerOptions.Standard.WithResolver(SpanFormatterResolver.Instance);
-            byte[] bytes = MessagePackSerializer.Serialize(traces, serializerOptions);
+            byte[] bytes = MessagePackSerializer.Serialize(traces, _serializerOptions);
 
             using (HttpContent content = new ByteArrayContent(bytes))
             {
+                content.Headers.ContentType = _mediaTypeHeaderValue;
                 content.Headers.Add(AgentHttpHeaderNames.TraceCount, traces.Count.ToString(CultureInfo.InvariantCulture));
                 HttpResponseMessage responseMessage = await _client.PostAsync(_tracesEndpoint, content).ConfigureAwait(false);
                 responseMessage.EnsureSuccessStatusCode();
