21da6390719b55dcd8972e2d4d629b3f47ab8ff3
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2025-02-21 18:20:09 -0500

    clean up in ProcessHelper, dispose the Process

--- src/Datadog.OpenTelemetry.Exporter/ProcessHelper.cs
+++ src/Datadog.OpenTelemetry.Exporter/ProcessHelper.cs
@@ -5,11 +5,12 @@ namespace Datadog.OpenTelemetry.Exporter;
 internal static class ProcessHelper
 {
     public static long ProcessId { get; }
+
     public static string ProcessName { get; }
 
     static ProcessHelper()
     {
-        var process = Process.GetCurrentProcess();
+        using var process = Process.GetCurrentProcess();
         ProcessId = process.Id;
         ProcessName = process.ProcessName;
     }
