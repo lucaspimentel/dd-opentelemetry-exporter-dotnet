15b955ee6c3ecc334fe72fcf436c72dd7a0c9e61
Lucas Pimentel <lucas.pimentel@datadoghq.com>
2025-02-21 18:25:14 -0500

    move http header name constants into TraceAgentClient

--- src/Datadog.OpenTelemetry.Exporter/TraceAgentClient.cs
+++ src/Datadog.OpenTelemetry.Exporter/TraceAgentClient.cs
@@ -12,6 +12,36 @@ namespace Datadog.OpenTelemetry.Exporter;
 
 public class TraceAgentClient
 {
+    private static class Headers
+    {
+        public const string Language = "Datadog-Meta-Lang";
+
+        /// <summary>
+        /// The interpreter for the given language, e.g. ".NET Framework", ".NET Core", or ".NET".
+        /// </summary>
+        public const string LanguageInterpreter = "Datadog-Meta-Lang-Interpreter";
+
+        /// <summary>
+        /// The interpreter version for the given language, e.g. "8.0" for .NET 8
+        /// </summary>
+        public const string LanguageVersion = "Datadog-Meta-Lang-Version";
+
+        /// <summary>
+        /// The version of the tracer that generated this span.
+        /// </summary>
+        public const string TracerVersion = "Datadog-Meta-Tracer-Version";
+
+        /// <summary>
+        /// The number of unique traces per request.
+        /// </summary>
+        public const string TraceCount = "X-Datadog-Trace-Count";
+
+        /// <summary>
+        /// The id of the container where the traced application is running.
+        /// </summary>
+        public const string ContainerId = "Datadog-Container-ID";
+    }
+
     private const string TracesPath = "/v0.4/traces";
     private static readonly string RuntimeVersion = Environment.Version.ToString();
     private static readonly MediaTypeHeaderValue MediaTypeHeaderValue = new("application/msgpack");
@@ -28,9 +58,9 @@ public class TraceAgentClient
         _tracesEndpoint = new Uri(new Uri(baseEndpoint), TracesPath);
 
         _client = new HttpClient();
-        _client.DefaultRequestHeaders.Add(AgentHttpHeaderNames.Language, "dotnet");
-        _client.DefaultRequestHeaders.Add(AgentHttpHeaderNames.LanguageInterpreter, ".NET");
-        _client.DefaultRequestHeaders.Add(AgentHttpHeaderNames.LanguageVersion, RuntimeVersion);
+        _client.DefaultRequestHeaders.Add(Headers.Language, "dotnet");
+        _client.DefaultRequestHeaders.Add(Headers.LanguageInterpreter, ".NET");
+        _client.DefaultRequestHeaders.Add(Headers.LanguageVersion, RuntimeVersion);
     }
 
     public async Task SendTracesAsync(IEnumerable<Span> spans)
@@ -48,7 +78,7 @@ public class TraceAgentClient
 
         using HttpContent content = new ByteArrayContent(bytes);
         content.Headers.ContentType = MediaTypeHeaderValue;
-        content.Headers.Add(AgentHttpHeaderNames.TraceCount, traces.Count.ToString(CultureInfo.InvariantCulture));
+        content.Headers.Add(Headers.TraceCount, traces.Count.ToString(CultureInfo.InvariantCulture));
 
         using var responseMessage = await _client.PostAsync(_tracesEndpoint, content).ConfigureAwait(false);
         responseMessage.EnsureSuccessStatusCode();
